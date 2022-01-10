2bbf3ebc410862384bd5ebe6bd12827604c8965a
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-10 14:18:23 -0800

    changed my app.js so it shows me a status to make sure its working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom/cjs/react-router-dom';
 import SignIn from './views/Auth/SignIn';
+import { useState } from 'react';
+import { getUser } from './services/users';
 
 import './App.css';
 
 function App() {
+  const [currentUser] = useState(getUser());
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact>
+          <Route path="/signin" exact>
             <SignIn />
+          </Route>
+          <Route path="/">
+            {currentUser && <h1>I am signed in!</h1>}
+            {!currentUser && <h1>I am not signed in!</h1>}
           </Route>
         </Switch>
       </BrowserRouter>
