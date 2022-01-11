45e5365016280111f8dc47194d9062de2ec26ad2
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-10 17:30:47 -0800

    added my auth page and css for the sign in and sing up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom/cjs/react-router-dom';
-import SignIn from './views/Auth/SignIn';
-import SignUp from './views/Auth/SignUp';
 import { useState } from 'react';
-import { getUser } from './services/users';
+import { getUser, logout } from './services/users';
+import Auth from './views/Auth/Auth';
 
 import './App.css';
 
 function App() {
-  const [currentUser] = useState(getUser());
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/signin" exact>
-            <SignIn />
-          </Route>
-          <Route path="/signup" exact>
-            <SignUp />
-          </Route>
-          <Route path="/">
-            {currentUser && <h1>I am signed in!</h1>}
-            {!currentUser && <h1>I am not signed in!</h1>}
+          <Route exact path="/">
+            {currentUser && (
+              <>
+                <h1>I am Signed In</h1>
+                <button onClick={logoutUser}>Log Out</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
           </Route>
         </Switch>
       </BrowserRouter>
